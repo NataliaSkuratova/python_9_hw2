cb46fa9a8b413874da2108f9ff654c8f7ed8b558
Natalia Skuratova <150360864+NataliaSkuratova@users.noreply.github.com>
2023-11-13 14:18:22 +0300

    Update test_google_search.py
    change value in config.window_width and config.window_height

--- test_google_search.py
+++ test_google_search.py
@@ -4,8 +4,8 @@ import pytest
 
 @pytest.fixture
 def browser_configs():
-    browser.config.window_width = 400
-    browser.config.window_height = 667
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
     yield
     browser.quit()
 
